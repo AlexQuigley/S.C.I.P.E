776becbeb9bdcb6b7e9813baea53f90e98d8c3ef
AlexQuigley <alexander.quigley2001@gmail.com>
2024-10-22 14:37:44 -0500

    Testing out module conversion

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -1,8 +1,11 @@
-module.exports = {
+import { Client, ChatInputCommandInteraction } from "discord.js";
+
+export default {
   name: 'ping',
   description: 'Replies with the bot ping!',
+  options: [], // Ensure options are defined, even if empty
 
-  callback: async (client, interaction) => {
+  callback: async (client: Client, interaction: ChatInputCommandInteraction) => {
     await interaction.deferReply();
 
     const reply = await interaction.fetchReply();
